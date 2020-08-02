d6619b0675f6291e88495e785a5fb43db8f81f3a
Arslaan Qadus <azy2k9@gmail.com>
2020-08-02 01:30:56 +0100

    Header layout

---
+++ client/src/layouts/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 3rem;
+  font-weight: bold;
+  padding: 36px 0px;
+`;
+
+const Header = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+Header.propTypes = {
+  children: PropTypes.string || PropTypes.arrayOf(PropTypes.node),
+};
+
+export default Header;
