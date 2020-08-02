ccfbd37257877e63e75ef1b3e05de539ebcf5731
Arslaan Qadus <azy2k9@gmail.com>
2020-08-02 01:29:44 +0100

    export atoms

---
+++ client/src/components/atoms/index.js
@@ -0,0 +1,7 @@
+import Button from './Button';
+import Card from './Card';
+import Input from './Input';
+import Radio from './Radio';
+import Title from './Title';
+
+export { Button, Card, Input, Radio, Title };
